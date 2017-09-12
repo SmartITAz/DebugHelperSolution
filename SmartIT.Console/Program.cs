184367b40085a6738c64ddc55da57876f3669f5a
smartitaz <eduorg@yahoo.com>
2017-09-12 14:54:52 -0700

    Initial commit

---
+++ SmartIT.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using SmartIT.DebugHelper;
+using System;
+namespace SmartIT.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            args.DDump("args");
+
+            Employee employee = new Employee() { Id = 1, Name = "Jen" };
+            employee.DDump("Employee");
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+} 
---
+++ SmartIT.DebugHelper/DebugHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace SmartIT.DebugHelper
+{
+    public static class DebugHelper
+    {
+        public static void CLog(string message, [CallerFilePath] string file = null, [CallerLineNumber] int line = 0)
+        {
+            Console.WriteLine("{0} ({1}): {2}", Path.GetFileName(file), line, message);
+        }
+
+        public static void DLog(string message, [CallerFilePath] string file = null, [CallerLineNumber] int line = 0)
+        {
+            Debug.WriteLine("{0} ({1}): {2}", Path.GetFileName(file), line, message);
+        }
+
+        public static void DDump(this object obj, string message)
+        {
+            DLog(message);
+            Debug.WriteLine(JsonConvert.SerializeObject(obj));
+        }
+    }
+}
